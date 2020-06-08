f8ad707594009395bc891214782e4f64180a7b77
Pavel Nosov <soundrussian@gmail.com>
2020-06-08 11:35:23 +0300

    Send credentials in axios requests

--- resources/js/http-common.js
+++ resources/js/http-common.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-  headers: { 'X-Requested-With': 'XMLHttpRequest' }
+  headers: { 'X-Requested-With': 'XMLHttpRequest' },
+  withCredentials: true
 })
