bb277eebf63ae490150f3ebd57002a8867019c14
Pavel Nosov <soundrussian@gmail.com>
2020-06-08 10:41:07 +0300

    Test for login API

--- resources/js/api/__tests___/api.spec.js
+++ resources/js/api/__tests___/api.spec.js
@@ -1,6 +1,6 @@
 import HTTP from 'http-common'
 import moxios from 'moxios'
-import { fetchCurrentUser } from '../api'
+import { fetchCurrentUser, login } from '../api'
 
 describe('api', () => {
   beforeEach(() => {
@@ -29,6 +29,34 @@ describe('api', () => {
       })
 
       return expect(fetchCurrentUser()).resolves.toEqual(expected)
+    })
+  })
+
+  describe('login', () => {
+    beforeEach(() => {
+      // mock request to /sanctum/csrf-cookie
+      moxios.stubRequest('/sanctum/csrf-cookie', () => {
+        status: 200
+      })
+    })
+
+    it('resolves with user if login is successful', () => {
+      const expected = { username: 'John Doe', role: 'student' }
+      moxios.stubRequest('/api/login', {
+        status: 200,
+        response: expected
+      })
+
+      return expect(login({})).resolves.toEqual(expected)
+    })
+
+    it('resolves with empty guest user if request unsuccessful', () => {
+      const expected = { username: '', role: 'guest' }
+      moxios.stubRequest('/api/login', {
+        status: 401
+      })
+
+      return expect(login({})).resolves.toEqual(expected)
     })
   })
 })
--- resources/js/api/api.js
+++ resources/js/api/api.js
@@ -1,9 +1,21 @@
 import HTTP from 'http-common'
 
+const blankUser = { username: '', role: 'guest' }
+
 export function fetchCurrentUser () {
   return new Promise((resolve) => {
     HTTP.get('/api/user')
       .then((resp) => resolve(resp.data))
-      .catch(() => resolve({ username: '', role: 'guest' }))
+      .catch(() => resolve(blankUser))
+  })
+}
+
+export function login (credentials) {
+  return new Promise((resolve) => {
+    HTTP.get('/sanctum/csrf-cookie').then(() => {
+      HTTP.post('/api/login', credentials)
+        .then((resp) => resolve(resp.data))
+        .catch(() => resolve(blankUser))
+    })
   })
 }
