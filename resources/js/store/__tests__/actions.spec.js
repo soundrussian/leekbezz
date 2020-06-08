9011f277523d9bbdfcd90e623922d797af9ae968
Pavel Nosov <soundrussian@gmail.com>
2020-06-08 10:44:30 +0300

    Login action

--- resources/js/store/__tests__/actions.spec.js
+++ resources/js/store/__tests__/actions.spec.js
@@ -1,5 +1,5 @@
 import actions from '../actions'
-import { fetchCurrentUser } from 'api/api'
+import { fetchCurrentUser, login } from 'api/api'
 import flushPromises from 'flush-promises'
 
 jest.mock('api/api')
@@ -15,6 +15,20 @@ describe('actions', () => {
       commit: jest.fn()
     }
     actions.fetchCurrentUser(context)
+    await flushPromises()
+    expect(context.commit).toHaveBeenCalledWith('setCurrentUser', { user })
+  })
+
+  test('login sets current user', async() => {
+    expect.assertions(1)
+    const user = { email: 'john.doe@example.com' }
+    login.mockImplementationOnce(() => {
+      return Promise.resolve(user)
+    })
+    const context = {
+      commit: jest.fn()
+    }
+    actions.login(context, {})
     await flushPromises()
     expect(context.commit).toHaveBeenCalledWith('setCurrentUser', { user })
   })
--- resources/js/store/actions.js
+++ resources/js/store/actions.js
@@ -1,7 +1,11 @@
-import { fetchCurrentUser } from 'api/api'
+import { fetchCurrentUser, login } from 'api/api'
 
 export default {
   fetchCurrentUser ({ commit }) {
     return fetchCurrentUser().then(user => commit('setCurrentUser', { user }))
+  },
+
+  login ({ commit }, credentials) {
+    return login(credentials).then(user => commit('setCurrentUser', { user }))
   }
 }
