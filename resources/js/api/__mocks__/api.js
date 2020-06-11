07dec52b5101a20e5f23a8f3044c9fb19168711f
Pavel Nosov <soundrussian@gmail.com>
2020-06-11 11:34:41 +0300

    Api and actions spec

--- resources/js/api/__mocks__/api.js
+++ resources/js/api/__mocks__/api.js
@@ -1,3 +1,4 @@
 export const fetchCurrentUser = jest.fn(() => Promise.resolve())
 export const login = jest.fn(() => Promise.resolve())
 export const logout = jest.fn(() => Promise.resolve())
+export const register = jest.fn(() => Promise.resolve())
